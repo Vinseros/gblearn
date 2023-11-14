b313c12ac399b6eda4525af83de3798d51f9017e
Stas <vins013@yandex.ru>
2023-11-14 20:09:50 +0300

    добавлена программа сортировки массива

---
+++ example12/Program.cs
@@ -0,0 +1,35 @@
+﻿// сортировка массива выбором максимального/минимального
+
+using System.Globalization;
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void  PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < array.Length; i ++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (arr[j] < arr[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
